89fcf09c0c180436158cecdbb0a5dc6cfea4f4b6
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-14 13:37:10 +0200

    initial commit

---
+++ Pari-e-Dispari/main.js
@@ -0,0 +1,37 @@
+// Pari e Dispari
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+
+const userChoice = (prompt('scegli pari o dispari'));
+console.log(userChoice, 'scelta utente');
+const userNumber = Number(prompt('scegli un numero da 1 a 5'));
+console.log(userNumber, 'numero utente');
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+function randomNumber(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const pcNumber = randomNumber(1,5);
+console.log(pcNumber, 'numero del pc');
+
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+function pariDispari (num_1, num_2){
+      const sum = num_1 + num_2;
+      console.log(sum, 'somma pari dispari');
+
+      if (sum % 2 === 0 ){
+            return 'pari';
+      } 
+      return 'dispari';
+}
+
+console.log('numero utente '+ userNumber);
+console.log('numero pc '+ pcNumber);
+
+if (pariDispari(pcNumber, userNumber) === userChoice){
+      alert ('user!');
+}else{
+      alert ('pc');
+}
+
